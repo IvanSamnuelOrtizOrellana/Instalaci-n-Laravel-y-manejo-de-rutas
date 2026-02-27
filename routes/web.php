fd8c814995a4ab2a1c8b5e6d8c790c6ff226936d
IvanSamnuelOrtizOrellana <ivanorore06@gmail.com>
2026-02-26 22:47:33 -0600

    commit 2 se agrego la ruta hola mundo y la vista hola, en routes y en la carpeta resources.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ContactoController; 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hola-mundo', function () {
+    return view('hola');
+});
+
+// Y aquí irán las rutas de tu formulario más adelante...
